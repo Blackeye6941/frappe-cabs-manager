32abdffdbd49c00cf5f24ccdf77a615adc3e8323
Blackeye6941 <leok37316@gmail.com>
2026-01-23 16:54:12 +0000

    Updated booking.js

--- rentals/rentals/doctype/booking/booking.js
+++ rentals/rentals/doctype/booking/booking.js
@@ -2,10 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Booking", {
+	onload(frm) {
+		console.log("Loaded..");
+	},
+	setup(frm) {
+		console.log("Setup");
+	},
 	refresh(frm) {
-	frm.add_custom_button("Accept", () => {
-		frm.set_value("status", "Accepted");
-		frm.save();
-	});
+		if(frm.doc.status == "New"){
+			frm.add_custom_button("Accept", () => {
+				frm.set_value("status", "Accepted");
+				frm.save();
+			}, "Actions");
+			frm.add_custom_button("Reject", () => {
+				frm.set_value("status", "Rejected");
+				frm.save();
+			}, "Actions");
+		}
 	},
 });

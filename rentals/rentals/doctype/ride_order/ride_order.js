8edac51f0815225fedc2d1b366173815f505350d
Blackeye6941 <leok37316@gmail.com>
2026-01-24 13:28:10 +0000

    Updated ride order

--- rentals/rentals/doctype/ride_order/ride_order.js
+++ rentals/rentals/doctype/ride_order/ride_order.js
@@ -1,7 +1,16 @@
 // Copyright (c) 2026, BlackEye and contributors
 // For license information, please see license.txt
 
-/*frappe.ui.form.on("Ride Order", {
+frappe.ui.form.on("Ride Order", {
 	refresh(frm) {
  	},
-});*/
+});
+
+frappe.ui.form.on('Ride Booking Item', {
+	refresh(frm) {
+		// your code here
+	},
+	distance(frm) {
+		console.log("Distance changed");
+	}
+})
